b7cec1ecf8c62d2040f07ff541838c76491a6a1c
YoussefAlsoufi <youssefalsoufi.1@gmail.com>
2024-06-29 21:33:29 +0200

    Use WTForms instead a simple request.form.get for more security and quick validation.

--- flask_app/routes/notes_route.py
+++ flask_app/routes/notes_route.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_required, current_user
+from flask_app.helper.note_form import NoteForm
 from flask_app.models import Note
 from flask_app import db
 from sqlalchemy.sql import func
@@ -9,17 +10,18 @@ notes = Blueprint('notes', __name__)
 @notes.route('/add', methods=['GET', 'POST'])
 @login_required
 def add_note():
-    if request.method == 'POST':
-        note_content = request.form.get('note')
-        if note_content.strip():
-            new_note = Note(note_data=note_content, user_id=current_user.id)
-            db.session.add(new_note)
-            db.session.commit()
-            flash('Note added successfully!', 'success')
-        else:
-            flash('Note cannot be empty.', 'danger')
-        return redirect(url_for('notes.add_note'))
+    form = NoteForm()
+    if form.validate_on_submit():      
+        note_content = form.note_data.data
+        new_note = Note(note_data=note_content, user_id=current_user.id)
+        db.session.add(new_note)
+        db.session.commit()
+        flash('Note added successfully!', 'success')
+        return redirect(url_for('notes.add_note'))  # Redirect after POST to prevent resubmission
+    else:
+        if form.errors:
+            flash('An error appears, please try again.', 'danger')
     
     # For GET request, fetch and display user's notes
     user_notes = Note.query.filter_by(user_id=current_user.id).all()
-    return render_template('notes.html', user=current_user, notes=user_notes)
+    return render_template('notes.html', user=current_user, notes=user_notes, form = form)
