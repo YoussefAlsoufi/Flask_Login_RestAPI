44374408f77b23bcd3c2ffd30b653fb3bad9a735
YoussefAlsoufi <youssefalsoufi.1@gmail.com>
2024-07-07 16:45:17 +0200

    add database integration test case to test_auth file

--- test_flask_app/test_auth_route.py
+++ test_flask_app/test_auth_route.py
@@ -2,6 +2,7 @@ import pytest
 from flask_app.models import User
 import logging
 from flask_app import db
+from test_helper.csrf_token_helper import get_csrf_token
 
 @pytest.mark.parametrize("data, expected_status, expected_flash", [
     (
@@ -15,7 +16,8 @@ from flask_app import db
         [b'This field is required.' ,
          b'Email must be a @gmail.com address.',
          b'Password must include at least one uppercase letter, one lowercase letter, one number, and one special character.',
-         b'Field must be equal to password.'] 
+         b'Field must be equal to password.',
+         b'Entere a valid phone number.'] 
     ),
     (
         {
@@ -30,18 +32,15 @@ from flask_app import db
     ),
 ])
 def test_signup(client, data, expected_status, expected_flash):
+    logging.info("test_signup started!")
     # If testing duplicate email, create a user with the email first
     if data['email'] == 'existing_user@gmail.com':
         user = User(email='existing_user@gmail.com', user_name='ExistingUser', phone='0999999999', password='password')
         db.session.add(user)
         db.session.commit()
-
-        # Get the CSRF token from the form
-    response = client.get('/sign-up')
-    csrf_token = response.data.decode().split('name="csrf_token" type="hidden" value="')[1].split('"')[0]
     
     # Add the CSRF token to the data
-    data['csrf_token'] = csrf_token
+    data['csrf_token'] = get_csrf_token(client, '/sign-up')
 
     # Submit the form with the CSRF token included
     response = client.post('/sign-up', data=data, follow_redirects=False)
@@ -64,15 +63,25 @@ def test_signup(client, data, expected_status, expected_flash):
             assert flash_message in response.data
 
 
-"""def test_signup_database_integration(client):
+def test_signup_database_integration(client):
+    logging.info("test_signup_database_integration started!")
     initial_user_count = User.query.count()
 
-    data = { 'email': 'test@gmail.com', 'user_name': 'Test', 'phone': '0999999999', 'password': 'Tt@123', 'confirm_password': 'Tt@123' }
-    response = client.post('/sign-up', data=data, follow_redirects=True)
+    data = { 'email': 'test@gmail.com',
+             'user_name': 'Test', 
+             'phone': '0999999999', 
+             'password': 'Tt@123', 
+             'confirm_password': 'Tt@123' 
+            }
+    data['csrf_token'] = get_csrf_token(client,'/sign-up')
+    response = client.post('/sign-up', data=data, follow_redirects=False)
 
-    assert response.status_code == 200  # Redirected after successful signup
+    assert response.status_code == 302, f"Expected status code 302, got {response.status_code}"
+    assert response.headers['Location'].startswith('/login')
     assert User.query.count() == initial_user_count + 1  # Verify that a new user is added to the database
 
+
+"""
 def test_signup_exception_handling(client, mocker):
     # Mock the database session to raise an exception
     mocker.patch('flask_app.db.session.add', side_effect=Exception('Database error'))
