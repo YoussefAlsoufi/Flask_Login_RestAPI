e4bf03ca58ebe161ca0dfde47b4e68e5670b5478
YoussefAlsoufi <youssefalsoufi.1@gmail.com>
2024-07-11 22:30:32 +0200

    enhance the update role test case

--- test_flask_app/test_login.py
+++ test_flask_app/test_login.py
@@ -2,8 +2,8 @@ from test_helper.signup_user import signup_user
 from test_helper.csrf_token_helper import get_csrf_token
 import logging 
 
-def test_login(client):
-    logging.info("Login test started !")
+def test_valid_login(client):
+    logging.info("test_valid_login test started !")
     # Create a user
     signup_user(client)
     logging.debug("a user has created in db.")
@@ -15,24 +15,38 @@ def test_login(client):
     }
     correct_data['csrf_token'] = get_csrf_token(client, '/login')
 
+    # Test with correct credentials
+    response = client.post('/login', data=correct_data, follow_redirects=False)
+    logging.debug("Response status code with correct creds: %s", response.status_code)
+    logging.debug("Response data with correct creds: %s", response.data)
+    assert response.status_code == 302, f"Expected status code 302 for redirect with correct creds, got {response.status_code}"
+
+    # Follow the redirect
+    response = client.get(response.headers['Location'], follow_redirects=True)
+    logging.debug("Follow redirect status code: %s", response.status_code)
+    logging.debug("Follow redirect data: %s", response.data)
+    assert response.status_code == 200, "Expected status code 200 after following redirect with correct creds"
+    assert b'Login successful!' in response.data, "Expected flash message 'Login successful!' not found in response data."
+
+
+def test_invalid_login(client):
+    logging.info("test_invalid_login test started !")
+    # Create a user
+    signup_user(client)
+    logging.debug("a user has created in db.")
+
     # Attempt to login with a incorerct user's creds: 
     incorrect_data = {
         'email' : 'test@gmail.com',
         'password' : 'TT@123',
     }
     incorrect_data['csrf_token'] = get_csrf_token(client, '/login')
-
-    response = client.post('/login', data= correct_data, follow_redirects = False)
-    logging.debug("Follow response status code with correct creds: %s", response.status_code)
-    logging.debug("Follow response data with correct creds is : %s", response.data)
-    assert response.status_code == 302, f"Expected status code 302 with correct creds, got {response.status_code}"
-
-    response = client.post('/login', data = incorrect_data, follow_redirects = True)
-    logging.debug("Follow response status code with incorrect creds: %s", response.status_code)
-    logging.debug("Follow response data with incorrect creds is : %s", response.data)
-    assert response.status_code == 200, f"Expected status code 200 're-rendered' with incorrect creds, got {response.status_code}"
     
-
-
+    # Test with incorrect credentials
+    response = client.post('/login', data=incorrect_data, follow_redirects=True)
+    logging.debug("Response status code with incorrect creds: %s", response.status_code)
+    logging.debug("Response data with incorrect creds: %s", response.data)
+    assert response.status_code == 200, f"Expected status code 200 're-rendered' with incorrect creds, got {response.status_code}"
+    assert b'Login unsuccessful. Please check email and password.' in response.data, "Expected flash message 'Login unsuccessful. Please check email and password.' not found in response data."
 
 
