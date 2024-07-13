a4cc34feec138bfe5d05abe40e3a92d62de72b28
YoussefAlsoufi <youssefalsoufi.1@gmail.com>
2024-07-13 23:21:46 +0200

    create a live chat template and live chat route

--- flask_app/helper/chat_room_form.py
+++ flask_app/helper/chat_room_form.py
@@ -1,8 +1,14 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired
 class ChatRoomform(FlaskForm):
     name = StringField('Email', validators=[DataRequired()])
     code = StringField('Room Code')
     join = SubmitField('Join The Room')
     create = SubmitField('Create a Chat Room')
+
+class LiveChatRoom(FlaskForm):
+    message = TextAreaField('Messages')
+    send = SubmitField('Send')
+
+    
--- flask_app/routes/chat_room_route.py
+++ flask_app/routes/chat_room_route.py
@@ -1,12 +1,14 @@
-from flask import Blueprint, render_template, request, flash
-from flask_app.helper.chat_room_form import ChatRoomform
+from flask import Blueprint, render_template, request, flash, url_for, redirect
+from flask_app.helper.chat_room_form import ChatRoomform, LiveChatRoom
 from flask_app.helper.chat_room_helper import generate_unique_code, rooms
-from flask_login import current_user
+from flask_login import current_user, login_required
 
 chat_room = Blueprint('chat_room', __name__)
-chat_room_template = 'chat_room.html'
+chat_room_template = 'chat_room_home.html'
+chat_room_live = 'chat_room_live.html'
 
 @chat_room.route('/chat_room', methods=['POST','GET'])
+@login_required
 def get_chat_room():
     form = ChatRoomform()
     if form.validate_on_submit:
@@ -20,19 +22,31 @@ def get_chat_room():
                 flash ("Enter the room code to join.", 'danger')
                 return render_template(chat_room_template, form=form)
             elif code not in rooms:
-                flash("The Room you want to join doesn't exist, please check the code.", "danger")
+                flash("The Room you want to join doesn't exist, please check the room code.", "danger")
                 return render_template(chat_room_template, form=form)
             
-            return render_template(chat_room_template, form=form)
+            return redirect(url_for('chat_room.get_live_chat'))
 
         elif create:
 
             room_code = generate_unique_code()
             rooms[room_code] = {"members":0, "messages":[]}
 
-            return f"Creating room with name: {current_user.user_name}"
-        
-        session["room"] = room_code
-        session["name"] = current_user.user_name
-        
-    return render_template(chat_room_template)
+            return redirect(url_for('chat_room.get_live_chat', code = room_code))
+    
+
+    return render_template(chat_room_template, form=form)
+
+
+@chat_room.route('/live_chat',methods = ['POST','GET'])
+@login_required
+def get_live_chat():
+    form = LiveChatRoom()
+    if not current_user.is_authenticated:
+        return redirect (url_for('auth.login'))
+    if current_user.role not in ("admin", "super-admin"): 
+        return redirect(url_for('home_bp.home'))
+
+    room_code = request.args.get('code')
+    return render_template(chat_room_live, form=form, room_code = room_code)
+    
